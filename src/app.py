bf57cf49dbf35e7dd35aa258f45ee4f5c40a77e9
ilof2 <ilof@tuta.io>
2021-04-28 21:46:37 +0200

    Added config class

--- src/app.py
+++ src/app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 
+from config import Config
 
 app = Flask(__name__)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(
+        host=Config.APP_HOST,
+        port=Config.APP_PORT,
+        debug=Config.DEBUG
+    )
---
+++ src/config.py
@@ -0,0 +1,20 @@
+from os import environ, path
+from dotenv import load_dotenv
+
+base_dir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(base_dir, ".env"))
+
+
+class Config:
+    DEBUG = environ.get("DEBUG", True)
+    APP_HOST = environ.get("APP_HOST", "localhost")
+    APP_PORT = environ.get("APP_PORT", 5000)
+    MONGO_USER = environ.get("MONGO_USER")
+    MONGO_PASSWORD = environ.get("MONGO_PASSWORD")
+    MONGO_DB_NAME = environ.get("MONGO_DB_NAME", "test")
+    MONGO_PORT = environ.get("MONGO_PORT", 27017)
+    MONGO_HOST = environ.get("MONGO_HOST", "localhost")
+    MONGO_URI = environ.get("MONGO_URI",
+                            f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB_NAME}")
+
+
